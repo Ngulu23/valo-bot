526bf9e5858a9a21f172f06dcfa04ff18871c388
Ngulu23 <133994878+Ngulu23@users.noreply.github.com>
2025-06-04 18:30:52 +0200

    Update riot_api.py

--- riot_api.py
+++ riot_api.py
@@ -1,26 +1,27 @@
 import aiohttp
-import json
-
-with open("config.json") as f:
-    CONFIG = json.load(f)
-
-API_KEY = CONFIG["HENRIK_API_KEY"]
+import os
 
 async def get_valo_rank(riot_id: str):
     try:
         name, tag = riot_id.split("#")
     except ValueError:
-        return None
+        print("Format Riot ID incorrect :", riot_id)
+        return None, None
 
     url = f"https://api.henrikdev.xyz/valorant/v1/mmr/eu/{name}/{tag}"
-    headers = {"Authorization": API_KEY}
+    API_KEY = os.environ.get("HENRIK_API_KEY")
+    # DO NOT add Bearer/Basic
+    headers = {
+        "Authorization": API_KEY
+    }
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as resp:
-            if resp.status != 200:
-                return None
+            print("URL appelée :", url)
+            print("Status code :", resp.status)
             data = await resp.json()
-            if data["status"] != 200 or not data.get("data"):
-                return None
-            return data["data"]["currenttierpatched"]
+            print("Réponse API :", data)
+            if resp.status != 200 or data.get("status", 0) != 200 or not data.get("data"):
+                return None, None
+            return data["data"]["currenttierpatched"], data["data"]["currenttier"]
 
